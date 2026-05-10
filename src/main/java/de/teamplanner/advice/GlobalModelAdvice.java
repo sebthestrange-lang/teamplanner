0b9dde40f7e712de4f5214d1863c77df75a3757a
Sebastian <sebthestrange@googlemail.com>
2026-05-10 20:31:11 +0200

    feat(version): update application version to 1.0.0 and add appVersion model attribute for dynamic version display

---
+++ src/main/java/de/teamplanner/advice/GlobalModelAdvice.java
@@ -0,0 +1,18 @@
+package de.teamplanner.advice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+public class GlobalModelAdvice {
+
+    private final BuildProperties buildProperties;
+
+    @ModelAttribute("appVersion")
+    public String appVersion() {
+        return buildProperties.getVersion().replace("-SNAPSHOT", "");
+    }
+}
